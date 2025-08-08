22428043937c12336889fc85961280eeba5962fd
Dotkras <aldo_24leo@hotmail.com>
2025-08-08 17:42:16 -0600

    privacidad.php

--- LibroAlbedrio Final github/privacidad.php
+++ LibroAlbedrio Final github/privacidad.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🔒Política de Privacidad</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -195,4 +194,5 @@ session_start();
 </footer>
 
 </body>
+
 </html>
